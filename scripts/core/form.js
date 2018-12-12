d2382c26f020cfd45961a8f18c91dd4def8b88e8
André Marques <andregm_7@hotmail.com>
2018-12-12 01:12:42 +0000

    UPD: Form integration (WIP)

--- scripts/core/form.js
+++ scripts/core/form.js
@@ -12,10 +12,16 @@ project.form = function () {
     },
     variables: function variables() {
       var view = this;
+      view.form = view.el.querySelector('form');
+      view.formNotification = view.el.querySelector('.formNotification');
     },
     dummy: function dummy() {
-      var view = this;
-      console.log('form');
+      var view = this; // console.log('form');
+      // view.form.addEventListener('submit', event => {
+      //     event && event.preventDefault();
+      //     console.log('submitted');
+      //     debugger
+      // })
     }
   };
 };
--- sources/modules/Form/form.js
+++ sources/modules/Form/form.js
@@ -14,12 +14,20 @@ project.form = (() => {
 
         variables() {
             const view = this;
+
+            view.form = view.el.querySelector('form');
+            view.formNotification = view.el.querySelector('.formNotification');
         },
 
         dummy() {
             const view = this;
 
-            console.log('form');
+            // console.log('form');
+            // view.form.addEventListener('submit', event => {
+            //     event && event.preventDefault();
+            //     console.log('submitted');
+            //     debugger
+            // })
         }
     }
 
